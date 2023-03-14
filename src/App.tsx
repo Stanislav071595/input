b3e9b9dc7f21e5fee9efd24ba52a7f02e639ec0d
ExStas_Poh <monax071595@gmail.com>
2023-03-14 14:50:01 +0300

    Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FullInput} from "./components/FullInput";
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState( [
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    )
+
+    const addMessage = (title: string) => {
+        let newMessage =  {message: title};
+        setMessage([newMessage, ...message])
+    }
+
+    let [title, setTitle] = useState('')
+
+    const onCLickButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+    }
+
+
+    return (
+        <div className="App">
+            {/*<FullInput addMessage={addMessage}/>*/}
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={'+'} callBack={onCLickButtonHandler}/>
+        {
+            message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })
+        }
+</div>
+)
+    ;
 }
 
 export default App;
